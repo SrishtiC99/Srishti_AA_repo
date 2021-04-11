930da80f6586d1e8d9f7ada05e9074e8a1ae5dbd
Srishti Kumari <iamsri9631@gmail.com>
2021-04-11 12:17:43 +0530

    C++ Program file created

---
+++ Program.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FAST ios::sync_with_stdio(false); cin.tie(0),cout.tie(0);
+//(a/b)%m =((a%m)*pow(b,m-2)%m)%m
+
+void Solve(){
+  cout<<"This Function does nothing!!\n";
+  return;
+}
+
+int main(){
+  FAST
+  int T;
+  cin>>T;
+  while(T--){
+     Solve();
+  }
+  return 0;
+}
